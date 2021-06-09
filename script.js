12dcd319f37999245295cc8807ff720900ed2c19
julileon10 <julio.leon.diaz1@gmail.com>
2021-06-09 13:34:50 -0400

    MVP version completed

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const FOOD_COLOR = "red";
 
 let gameOn = true;
 
+let difficulty = 150;
+
+const lostModal = document.querySelector(".bg-modal-lost");
+
 const changeState = (block) => {
     if (block.classList.contains("false")) {
         block.classList.remove("false");
@@ -16,111 +20,6 @@ const changeState = (block) => {
         block.classList.add("false");
     }
 };
-
-class Snake {
-    constructor () {
-        this.x = 12;
-        this.y = 7;
-        this.vel = 1;
-        this.snake = [
-            [this.x, this.y],
-        ];
-        this.color = "green";
-        this.snakeHead = this.snake[0];
-        this.eat = false;
-    }
-    createSnake () {
-        const snakeSquare = document.querySelector(`.row-${this.snake[0][1]}_col-${this.snake[0][0]}`);
-        snakeSquare.classList.add("snake");
-        snakeSquare.style.backgroundColor = this.color;
-        changeState(snakeSquare);
-    }
-    move(direction) {
-        
-        if (direction === "up") {
-            if (this.snake.length > 1) {
-                for (let i = this.snake.length - 1; i > 0; i--) {
-                    this.snake[i][0] = this.snake[i - 1][0];
-                    this.snake[i][1] = this.snake[i - 1][1];
-                }
-                this.snake[0][1] -= this.vel;
-            } else {
-                this.snake[0][1] -= this.vel;
-            }
-        }
-        if (direction === "down") {
-            if (this.snake.length > 1) {
-                for (let i = this.snake.length - 1; i > 0; i--) {
-                    this.snake[i][0] = this.snake[i - 1][0];
-                    this.snake[i][1] = this.snake[i - 1][1];
-                }
-                this.snake[0][1] += this.vel;
-            } else {
-                this.snake[0][1] += this.vel;
-            }
-        }
-        if (direction === "right") {
-            if (this.snake.length > 1) {
-                for (let i = this.snake.length - 1; i > 0; i--) {
-                    this.snake[i][0] = this.snake[i - 1][0];
-                    this.snake[i][1] = this.snake[i - 1][1];
-                }
-                this.snake[0][0] += this.vel;
-            } else {
-                this.snake[0][0] += this.vel;
-            }
-        }
-        if (direction === "left") {
-            if (this.snake.length > 1) {
-                for (let i = this.snake.length - 1; i > 0; i--) {
-                    this.snake[i][0] = this.snake[i - 1][0];
-                    this.snake[i][1] = this.snake[i - 1][1];
-                }
-                this.snake[0][0] -= this.vel;
-            } else {
-                this.snake[0][0] -= this.vel;
-            }
-        }
-    }
-    snakeAte(board) {
-        this.snake.push([board.food[0], board.food[1]]);
-        // console.log(this.snake.length);
-        board.food.pop();
-        board.food.pop();
-        document.querySelector(".food").classList.remove("food");
-        this.eat = false;
-    }
-    update(board) {
-        if (this.eat) {
-            this.snakeAte(board);
-        }
-        
-        try {
-            if (document.querySelector(`.row-${this.snake[0][1]}_col-${this.snake[0][0]}`).classList.contains("snake")) {
-                console.log("You crashed!");
-                // gameOn = false;
-            }
-        } catch (err) {
-            console.log("ignore this");
-        }
-
-        for (let square of document.querySelectorAll(".snake")) {
-            square.classList.remove("snake");
-        }
-        this.snake.forEach((block) => {
-            try {
-                document.querySelector(`.row-${block[1]}_col-${block[0]}`).classList.add("snake");
-            } catch (err) {
-                console.log("You went out of bounds!");
-                // gameOn = false;
-            }
-        });
-
-        if (this.snake[this.snake.length - 1][0] === board.food[0] && this.snake[this.snake.length - 1][1] === board.food[1]) {
-            this.eat = true;
-        }
-    }
-}
 
 class Board {
     constructor () {
@@ -142,6 +41,8 @@ class Board {
         }
     }
     update() {
+        // console.log("BOARD");
+        // if (!gameOn) return;
         const squares = document.querySelectorAll(".square");
         for (let square of squares) {
             if (square.classList.contains("snake")) {
@@ -157,8 +58,8 @@ class Board {
         }
     }
     createFood() {
-        const x = Math.ceil(Math.random() * 25);
-        const y = Math.ceil(Math.random() * 50);
+        let x = Math.ceil(Math.random() * 25);
+        let y = Math.ceil(Math.random() * 50);
 
         while (document.querySelector(`.row-${x}_col-${y}`).classList.contains("snake")) {
             x++;
@@ -173,31 +74,244 @@ class Board {
     }
 }
 
-const newBoard = new Board();
-const newSnake = new Snake();
+class Snake {
+    constructor (highestScore=1) {
+        this.x = 12;
+        this.y = 7;
+        this.vel = 1;
+        this.dir = "right";
+        this.snake = [
+            [this.x, this.y],
+        ];
+        this.color = "green";
+        this.snakeHead = this.snake[0];
+        this.eat = false;
+        this.highestScore = highestScore;
+    }
+    getScore() {
+        return this.snake.length;
+    }
+    updateHighestScore() {
+        if (this.getScore() > this.highestScore) {
+            this.highestScore = this.getScore();
+        }
+    }
+    getHighestScore() {
+        return this.highestScore;
+    }
+    createSnake () {
+        const snakeSquare = document.querySelector(`.row-${this.snake[0][1]}_col-${this.snake[0][0]}`);
+        snakeSquare.classList.add("snake");
+        snakeSquare.style.backgroundColor = this.color;
+        changeState(snakeSquare);
+    }
+    move(direction) {
+        if (!gameOn) return;
+        // console.log("MOVE");
+        if (direction === "ArrowUp" && this.dir !== "down") {
+            if (this.snake.length > 1) {
+                for (let i = this.snake.length - 1; i > 0; i--) {
+                    this.snake[i][0] = this.snake[i - 1][0];
+                    this.snake[i][1] = this.snake[i - 1][1];
+                }
+                this.snake[0][1] -= this.vel;
+            } else {
+                this.snake[0][1] -= this.vel;
+            }
+            this.dir = "up";
+        }
+        if (direction === "ArrowDown" && this.dir !== "up") {
+            if (this.snake.length > 1) {
+                for (let i = this.snake.length - 1; i > 0; i--) {
+                    this.snake[i][0] = this.snake[i - 1][0];
+                    this.snake[i][1] = this.snake[i - 1][1];
+                }
+                this.snake[0][1] += this.vel;
+            } else {
+                this.snake[0][1] += this.vel;
+            }
+            this.dir = "down";
+        }
+        if (direction === "ArrowRight" && this.dir !== "left") {
+            if (this.snake.length > 1) {
+                for (let i = this.snake.length - 1; i > 0; i--) {
+                    this.snake[i][0] = this.snake[i - 1][0];
+                    this.snake[i][1] = this.snake[i - 1][1];
+                }
+                this.snake[0][0] += this.vel;
+            } else {
+                this.snake[0][0] += this.vel;
+            }
+            this.dir = "right";
+        }
+        if (direction === "ArrowLeft" && this.dir !== "right") {
+            if (this.snake.length > 1) {
+                for (let i = this.snake.length - 1; i > 0; i--) {
+                    this.snake[i][0] = this.snake[i - 1][0];
+                    this.snake[i][1] = this.snake[i - 1][1];
+                }
+                this.snake[0][0] -= this.vel;
+            } else {
+                this.snake[0][0] -= this.vel;
+            }
+            this.dir = "left";
+        }
+        console.log(this.dir);
+    }
+    snakeAte(board) {
+        this.snake.push([board.food[0], board.food[1]]);
+        board.food.pop();
+        board.food.pop();
+        document.querySelector(".food").classList.remove("food");
+        this.eat = false;
+    }
+    update(board, moveIntervalID, snakeIntervalID, boardIntervalID) {
+        // console.log("SNAKE");
+        // if (!gameOn) return;
+
+        if (this.eat) {
+            this.snakeAte(board);
+        }
+        
+        try {
+            if (document.querySelector(`.row-${this.snake[0][1]}_col-${this.snake[0][0]}`).classList.contains("snake")) {
+                console.log("You crashed!");
+                window.clearInterval(moveIntervalID);
+                window.clearInterval(snakeIntervalID);
+                window.clearInterval(boardIntervalID);
+                lostModal.style.display = "flex";
+                gameOn = false;
+            }
+        } catch (err) {
+            console.log("ignore this");
+        }
+
+        for (let square of document.querySelectorAll(".snake")) {
+            square.classList.remove("snake");
+        }
+        this.snake.forEach((block) => {
+            try {
+                document.querySelector(`.row-${block[1]}_col-${block[0]}`).classList.add("snake");
+            } catch (err) {
+                console.log("You went out of bounds!");
+                window.clearInterval(moveIntervalID);
+                window.clearInterval(snakeIntervalID);
+                window.clearInterval(boardIntervalID);
+                lostModal.style.display = "flex";
+                gameOn = false;
+            }
+        });
+
+        if (this.snake[this.snake.length - 1][0] === board.food[0] && this.snake[this.snake.length - 1][1] === board.food[1]) {
+            this.eat = true;
+        }
+    }
+}
+
+let newBoard = new Board();
+let newSnake = new Snake();
 newSnake.createSnake();
+newBoard.createFood();
 
-document.addEventListener('keydown', (event) => {
-    var key = event.key;
+const scoreDisplay = document.querySelector(".current-score");
+const highestScoreDisplay = document.querySelector(".highest-score");
 
-    if (key === "ArrowLeft") {
-        newSnake.move("left");
-    }
-    if (key === "ArrowRight") {
-        newSnake.move("right");
-    }
-    if (key === "ArrowDown") {
-        newSnake.move("down");
-    }
-    if (key === "ArrowUp") {
-        newSnake.move("up");
+let moveIntervalID = null;
+let snakeIntervalID = null;
+let boardIntervalID = null;
+let currentScoreIntervalID = null;
+let highestScoreIntervalID = null;
+
+const restartBar = document.querySelector(".difficulty-selector");
+
+const selectDifficulty = (event) => {
+
+    if (event.target.classList.contains("easy")) {
+        difficulty = 200;
     }
 
-    // while (gameOn) {
-        newSnake.update(newBoard);
+    if (event.target.classList.contains("medium")) {
+        difficulty = 150;
+    }
+
+    if (event.target.classList.contains("hard")) {
+        difficulty = 100;
+    }
+
+    if (event.target.classList.contains("insane")) {
+        difficulty = 50;
+    }
+
+};
+
+document.body.addEventListener('keyup', (event) => {
+
+    window.clearInterval(moveIntervalID);
+    window.clearInterval(snakeIntervalID);
+    window.clearInterval(boardIntervalID);
+    window.clearInterval(currentScoreIntervalID);
+    window.clearInterval(highestScoreIntervalID);
+
+    moveIntervalID = window.setInterval(() => {
+        newSnake.move(event.key);
+    }, difficulty);
+    snakeIntervalID = window.setInterval(() => {
+        newSnake.update(newBoard, moveIntervalID, snakeIntervalID, boardIntervalID);
+    }, difficulty);
+    boardIntervalID = window.setInterval(() => {
         newBoard.update();
-    // }
-
+    }, difficulty);
+    currentScoreIntervalID = window.setInterval(() => {
+        scoreDisplay.innerText = newSnake.getScore();
+    });
+    highestScoreIntervalID = window.setInterval(() => {
+        newSnake.updateHighestScore();
+        highestScoreDisplay.innerText = newSnake.getHighestScore();
+    });
 });
 
+const form = document.querySelector(".player-name-form");
+const modal = document.querySelector(".bg-modal");
 
+const setPlayerName = (event) => {
+    event.preventDefault();
+
+    const playerNameInput = document.querySelector(".player-name-input");
+    const playerNameDisplay = document.querySelector(".player-name");
+
+    playerNameDisplay.innerText = playerNameInput.value;
+
+    modal.style.display = "none";
+};
+
+const restartGame = () => {
+
+    const boardSquares = document.querySelectorAll(".square");
+    const gameBoard = document.querySelector(".game-board");
+
+    boardSquares.forEach((square) => {
+        gameBoard.removeChild(square);
+    });
+
+    newBoard = new Board();
+    newSnake = new Snake(Number(highestScoreDisplay.innerText));
+    newSnake.createSnake();
+    newBoard.createFood();
+
+    gameOn = true;
+};
+
+form.addEventListener("submit", setPlayerName);
+
+const restartButton = document.querySelector(".restart-button");
+
+restartButton.addEventListener(("click"), restartGame);
+
+const lostRestartButton = document.querySelector(".restart-lost-button");
+
+const restartOnLost = () => {
+    restartGame();
+    lostModal.style.display = "none";
+}
+
+lostRestartButton.addEventListener("click", restartOnLost);
